b5005d6167e48093b4303d54d93235a0a922641e
Gisele Macieira <giselefchaves@gmail.com>
2023-10-19 15:06:21 -0300

    layout da parte de pagamento

--- src/pages/Dashboard/Payment/index.jsx
+++ src/pages/Dashboard/Payment/index.jsx
@@ -1,7 +1,9 @@
 import PaymentInfo from "../../../components/Dashboard/PaymentInfo";
+import PaymentDetails from "../../../components/Dashboard/PaymentDetails";
 
 export default function Payment() {
   return (
-    <PaymentInfo/>
+    //<PaymentInfo />
+    <PaymentDetails />
   );
 }
