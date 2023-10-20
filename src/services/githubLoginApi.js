e6afdf0bb52128d577ea842ed49cc7233d6c8bf4
GDMeira <gabrieldega970@gmail.com>
2023-10-20 19:42:30 -0300

    feat: adição dos cabeçalhos de resposta

--- src/services/githubLoginApi.js
+++ src/services/githubLoginApi.js
@@ -10,5 +10,11 @@ export async function getGithubCode() {
 
 export async function signInGithub(code) {
     const response = await backEnd.post('/auth/github/sign-in', { code });
+    response.headers("Access-Control-Allow-Origin", "*");
+    response.headers("Access-Control-Allow-Credentials", "true");
+    response.headers("Access-Control-Max-Age", "1800");
+    response.headers("Access-Control-Allow-Headers", "content-type");
+    response.headers("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+
     return response.data;
 }
